9bd366f1b05950c0f3f139900da31d06b72be7a0
dancreightondev <22957063+dancreightondev@users.noreply.github.com>
2025-07-30 20:40:24 +0100

    Flesh out testimonial styling

--- src/routes/home/tabs/AboutTab.tsx
+++ src/routes/home/tabs/AboutTab.tsx
@@ -11,18 +11,20 @@ export const AboutTab: FC<AboutTabProps> = ({ testimonials, className, ...props 
   return (
     <div
       id="about-tab"
-      className={twClassMerge(className, 'flex flex-col items-center justify-center h-full')}
+      className={twClassMerge(className, 'flex flex-col items-center justify-center h-full gap-8')}
       {...props}
     >
       <div id="people"></div>
       <div id="mission"></div>
-      <div id="testimonials">
-        <div className="grid grid-cols-3 gap-4">
-          <Testimonial data={testimonials && testimonials[0]} className="col-span-2 col-start-2" />
-          <Testimonial data={testimonials && testimonials[1]} className="col-span-2 col-start-1" />
-          <Testimonial data={testimonials && testimonials[2]} className="col-span-2 col-start-2" />
+      {testimonials && (
+        <div id="testimonials">
+          <div className="grid grid-cols-3 gap-y-6">
+            <Testimonial data={testimonials[0]} className="col-span-2 col-start-2" />
+            <Testimonial data={testimonials[1]} className="col-span-2 col-start-1" />
+            <Testimonial data={testimonials[2]} className="col-span-2 col-start-2" />
+          </div>
         </div>
-      </div>
+      )}
       <div id="cta"></div>
     </div>
   )
--- src/routes/home/tabs/about/Testimonial.tsx
+++ src/routes/home/tabs/about/Testimonial.tsx
@@ -1,9 +1,10 @@
 import { FC } from 'react'
 import { twClassMerge } from '~/utils/tailwind'
+import { BiSolidQuoteAltLeft, BiSolidQuoteAltRight } from 'react-icons/bi'
 
 interface TestimonialProps extends React.HTMLAttributes<HTMLDivElement> {
   // Custom props go here
-  data?: TestimonialData
+  data: TestimonialData
 }
 
 export interface TestimonialData {
@@ -13,11 +14,17 @@ export interface TestimonialData {
 
 export const Testimonial: FC<TestimonialProps> = ({ data, className, ...props }) => {
   return (
-    <div
-      className={twClassMerge(className, 'rounded-lg bg-primary-400 text-center p-2')}
-      {...props}
-    >
-      {data?.quote}
+    <div className={twClassMerge(className, 'relative')} {...props}>
+      <div className="absolute rounded-lg bg-primary-400 text-center p-2 size-full" />
+      <span className="absolute w-auto h-12 -left-3 -top-3 text-primary-600">
+        <BiSolidQuoteAltLeft className="size-full" />
+      </span>
+      <span className="absolute w-auto h-12 -right-3 -bottom-3 text-primary-600">
+        <BiSolidQuoteAltRight className="size-full" />
+      </span>
+      <p id="quote" className="relative z-10 p-8 select-text">
+        {data.quote}
+      </p>
     </div>
   )
 }
