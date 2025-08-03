e43a04bdea426a1bffe9bbf3fb59067c3126a443
dancreightondev <22957063+dancreightondev@users.noreply.github.com>
2025-08-03 19:55:46 +0100

    Flesh out event cards

--- src/routes/home/tabs/events/EventCard.tsx
+++ src/routes/home/tabs/events/EventCard.tsx
@@ -7,10 +7,32 @@ interface EventCardProps extends React.HTMLAttributes<HTMLDivElement> {
   event: Event
 }
 
-export const EventCard: FC<EventCardProps> = ({ className, ...props }) => {
+export const EventCard: FC<EventCardProps> = ({ event, className, ...props }) => {
   return (
-    <div className={twClassMerge(className)} {...props}>
-      {/* Component content goes here */}
+    <div
+      className={twClassMerge(className, 'bg-primary-400 rounded-lg p-2 flex flex-col gap-2')}
+      {...props}
+    >
+      <img src="https://placehold.co/512x256" className="w-full rounded-sm" />
+      <div>
+        <h3 className="font-bold">{event.name}</h3>
+        <p className="text-sm">{event.summary}</p>
+      </div>
+      <p className="text-xs text-primary-700">
+        {event.start.toLocaleDateString() === event.end.toLocaleDateString()
+          ? event.start.toLocaleDateString()
+          : `${event.start.toLocaleDateString()} - ${event.end.toLocaleDateString()}`}
+      </p>
+      <div className="flex justify-center">
+        <a
+          href={event.url}
+          className="inline-block text-sm text-center px-6 py-2 w-full bg-primary-700 text-white rounded hover:bg-primary-800 no-underline transition-colors"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Book tickets & find out more
+        </a>
+      </div>
     </div>
   )
 }
